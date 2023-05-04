fe16d508c0480aa2f3ac9648bcfc72fdcf430b5c
MahmoudAtef2 <matef3431@gmail.com>
2023-05-04 11:23:55 -0700

    teast this task

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to change a numper.
+ * @index:the index of  bit to set to (1).
+ *
+ * Return: 1 equal success, -1 equal erorr.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+
+int clear_bit(unsigned long int *number, int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	unsigned long int mask = ~(1UL << index);
+	*number = (*number & mask);
+		return (1);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this my readme file for task 0x14-bit_manipulation
