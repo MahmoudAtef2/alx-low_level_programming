10dd871bbe29e4ef3953354918e5def7ffafe4cd
MahmoudAtef2 <matef3431@gmail.com>
2023-06-14 13:30:32 -0700

    0x09-static_libraries/

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Function that writes a single character c.
+ * @c: The character.
+ *
+ * Return: the charscter to be printed.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
